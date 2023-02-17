122f0d1b05f04a77e9c759dee815fb314faf3a1d
Martial Domche <104767406+MarDom15@users.noreply.github.com>
2023-02-17 13:04:12 +0100

    premier essaie

---
+++ essaie.py
@@ -0,0 +1,4 @@
+nbre1 = float (input ("Entrez le premier nombre : "))
+nbre2 = float (input ("Entrez le deueime nombre : "))
+somme = nbre1 + nbre2
+print ("La somme de", nbre1, "et", nbre2, "est egale a", somme)
